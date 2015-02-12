52533bed280378cfa9c07ff0ea401625ddcccdc4
Easy Andre <Easy@pc14.home>
2015-02-12 09:15:04 +0100

    Initial Commit

---
+++ PricerEDPBS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PricerEDPBS
+//
+//  Created by  Easy Andre on 12/02/2015.
+//  Copyright (c) 2015 Ezechiel Andre. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
